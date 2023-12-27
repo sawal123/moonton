9ff7f1aae586d0b350cd07e1210e68ed805f206c
sawal123 <53683012+sawal123@users.noreply.github.com>
2023-12-27 10:13:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -53,25 +53,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.dashboar
     // Route::resource('movie/create', AdminController::class);
 });
 
-Route::prefix('prototype')->name('prototype')->group(function () {
-    Route::get('/login', function () {
-        return inertia::render('Prototype/Login');
-    })->name('.login');
 
-    Route::get('/register', function () {
-        return inertia::render('Prototype/Register');
-    })->name('.register');
-    Route::get('/dashboard', function () {
-        return inertia::render('Prototype/Dashboard');
-    })->name('.dashboard');
-    Route::get('/subcriptionPlan', function () {
-        return inertia::render('Prototype/subcriptionPlan');
-    })->name('.subcriptionPlan');
-
-    Route::get('/movie/{slug}', function () {
-        return inertia::render('Prototype/Movie/Show');
-    })->name('.movie.show');
-});
 
 
 
